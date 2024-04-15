e94fd72fd305382d68b1e9540090494901c50e4a
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-15 14:52:09 -0600

    feat: filter products depending on the filters that were set

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react'
 import Products from './components/Products.jsx'
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
 
 function App() {
 
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  });
+
+  const filterProducts = (products) => {
+    return products.filter( product => {
+      return(
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all'|| product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products);
+
   return (
     <>
-      <Products products={products} />
+      <Products products={filteredProducts} />
     </>
   )
 }
