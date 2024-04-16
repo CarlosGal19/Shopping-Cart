6d13006d92a2030c98bc8ac1b51cd892471f8d6d
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-16 12:23:25 -0600

    style: remove filter info from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,11 @@
-import { useFilters } from '../hooks/useFilters';
 import './Footer.css';
 
 export default function Footer() {
 
-    const { filters } = useFilters();
-
     return(
         <>
             <footer className="footer">
-                {
-                    JSON.stringify(filters, null, 2)
-                }
+                <p>© 2024 - All rights reserved</p>
             </footer>
         </>
     )
