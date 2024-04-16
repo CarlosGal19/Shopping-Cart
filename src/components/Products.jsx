d5f8a708a773d20d3804010b9c326348e001e8a0
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-16 12:58:46 -0600

    feat: render image depending on if the product is already in cart

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
-import { AddToCartIcon } from './Icons.jsx';
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons.jsx';
 import { useCart } from '../hooks/useCart';
 import './Products.css'
 
 function Products ({ products }) {
 
-    const { addToCart } = useCart();
+    const { cart, addToCart, removeFromCart } = useCart();
+
+    const checkCart = product => {
+        return cart.some(p => p.id === product.id)
+    }
+
+    console.log(cart);
 
     return(
         <main className='products'>
             <ul>
                 {
                     products.map(product => {
+                        const isOnCart = checkCart(product);
                         return(
                             <li key={product.id}>
                                 <img src={product.thumbnail} alt={product.title} />
@@ -19,8 +26,12 @@ function Products ({ products }) {
                                     <strong>{product.title}</strong> - ${product.price}
                                 </div>
                                 <div>
-                                    <button onClick={() => {addToCart(product)}}>
-                                        <AddToCartIcon />
+                                    <button onClick={() => isOnCart ? removeFromCart(product) : addToCart(product)}>
+                                        {
+                                            isOnCart
+                                            ? <RemoveFromCartIcon />
+                                            : <AddToCartIcon />
+                                        }
                                     </button>
                                 </div>
                             </li>
